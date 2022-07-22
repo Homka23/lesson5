04097a4f91f87abd69c5948ded9537b28712ff20
Homka23 <homka23052005@gmail.com>
2022-07-22 23:27:19 +0200

    Initial commit

---
+++ HomeWork/main.js
@@ -0,0 +1,47 @@
+// Ex 1
+
+
+// const hello = 'hello';
+// const owu = 'owu';
+// const com = 'com';
+// const ua = 'ua';
+// const n1 = 1;
+// const n2 = 10;
+// const n3 = -999;
+// const n4 = 123;
+// const n5 = 3.14;
+// const n6 = 2.7;
+// const n7 = 16;
+// const bool1 = true;
+// const bool2 = false;
+// const space = ', '
+// console.log(hello, owu, com, ua, n1, n2, n3, n4, n5, n6, n7, bool1, bool2);
+// alert(hello + space + owu + space + com + space + ua + space + n1 + space +
+//     n2 + space + n3 + space + n4 + space + n5 + space + n6 + space + n7 + space +
+//     bool1 + space + bool2);
+// document.write((hello + space + owu + space + com + space + ua + space + n1 + space +
+//     n2 + space + n3 + space + n4 + space + n5 + space + n6 + space + n7 + space +
+//     bool1 + space + bool2));
+
+// Ex 2
+
+const firstName = 'Anna';
+const middleName = 'Romanivna';
+const lastName = 'Homutnyk';
+const age = 17;
+
+const person = firstName + middleName + lastName;
+
+// Ex 3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof(a), typeof(b), typeof(c))
+
+// Ex 4
+
+console.log(prompt('Enter your name'));
+console.log(prompt('Enter your middle-name'));
+console.log(prompt('Enter your age'));
